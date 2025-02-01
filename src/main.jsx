e1b87f39234a7b0cc0e15696f276e4e93a01a327
Jhon Tapia <jhontapia_1511@outlook.com>
2025-01-31 23:07:43 -0600

    make button but error

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import ButtonCrearCuenta from "./buttons/button-crear-cuenta"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
@@ -5,6 +6,8 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div>E- commerce proyecto en cero</div>
+    <div>E- commerce proyecto en cero
+      <ButtonCrearCuenta></ButtonCrearCuenta>
+    </div>
   </StrictMode>,
 )

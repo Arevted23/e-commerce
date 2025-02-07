85f55ce73560520c73ef6f902b912496aef473ec
Hilario Arevalo <hilarioarevalo03@gmail.com>
2025-02-06 21:33:44 -0600

    feat(): header composition [ECP-8]

---
+++ src/composition/header/header.jsx
@@ -0,0 +1,9 @@
+// import { Button } from "../../common/button/Button.jsx";
+import { Navigation } from "../../common/navigation/navigation.jsx";
+export const Header = () => {
+  return (
+    <>
+      <Navigation />
+    </>
+  );
+};

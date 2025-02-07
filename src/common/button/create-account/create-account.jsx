3ae55c15652fae2bee1f830d12a01a8eac84b29c
Hilario Arevalo <hilarioarevalo03@gmail.com>
2025-02-06 21:33:24 -0600

    feat(): composition button: account and login [ECP-8]

---
+++ src/common/button/create-account/create-account.jsx
@@ -0,0 +1,10 @@
+import '../Button.css';
+export const CreateAccountButton = () => {
+  return (
+    <>
+      <a href="destino.html" className="boton-redireccion">
+        Crear una
+      </a>
+    </>
+  );
+};
---
+++ src/common/button/login-account/login-acount.jsx
@@ -0,0 +1,10 @@
+import '../Button.css';
+export const LoginAccount = () => {
+  return (
+    <>
+      <a href="destino.html" className="boton-redireccion">
+        Crear una
+      </a>
+    </>
+  );
+};
